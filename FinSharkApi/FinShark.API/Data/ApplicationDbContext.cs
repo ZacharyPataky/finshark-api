e2db747512004d8f3efe3d4503d8533a5cfc1c14
Zachary Pataky <zacharypataky@gmail.com>
2025-02-15 12:49:55 -0500

    PROGRESS/UPGRADE: Added DB context and migrated DB; ugraded to .NET 9.0

---
+++ FinSharkApi/FinShark.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FinShark.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinShark.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+    { }
+
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
