be3213a5515bb9951d9b4d62651789dc48cef640
Zachary Pataky <zacharypataky@gmail.com>
2025-02-15 13:01:57 -0500

    PROGRESS: Created basic Stock Controller.

---
+++ FinSharkApi/FinShark.API/Controllers/StockController.cs
@@ -0,0 +1,33 @@
+﻿using FinShark.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinShark.API.Controllers;
+
+[ApiController]
+[Route("api/stock")]
+public class StockController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var stocks = _context.Stocks.ToList();
+        return Ok(stocks);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        var stock = _context.Stocks.Find(id);  // Checks by the primary key
+
+        return stock != null ?
+            Ok(stock) :
+            NotFound();
+    }
+}
